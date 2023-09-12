df35fe605feb6ef97d4afa8685ef1a977cc088b0
Mert Eray <meerteray@gmail.com>
2023-09-12 23:09:23 +0300

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Mert Eray on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
