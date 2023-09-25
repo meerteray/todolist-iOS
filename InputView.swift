7222df7d62f8bbd2842713fb5f726bf2e45380e1
Mert Eray <meerteray@gmail.com>
2023-09-26 00:19:45 +0300

    InputView

---
+++ InputView.swift
@@ -0,0 +1,20 @@
+//
+//  InputView.swift
+//  todolist
+//
+//  Created by Mert Eray on 26.09.2023.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
