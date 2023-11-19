573501baecf387ecc06479c7e0b1b66444601b15
Mert Eray <meerteray@gmail.com>
2023-11-19 19:48:45 +0300

    new files added

---
+++ todolist/Views/ToDoListItemsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ToDoListItemsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ToDoListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListItemsView()
+    }
+}
