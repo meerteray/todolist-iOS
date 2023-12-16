29d69cbff86ddee81438cdaa3941f2fb25c78a8a
Mert Eray <meerteray@gmail.com>
2023-12-16 14:55:22 +0300

    Login Form added

--- todolist/Views/LoginView.swift
+++ todolist/Views/LoginView.swift
@@ -1,16 +1,41 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
            HeaderView()
                 
             // Login Form
-                
-            
+            Form{
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                Button {
+                 //   Attempt log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log in")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
+
+            }
             
             // Create Account
+            
+            
+            
             
             Spacer()
             }
